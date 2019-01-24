8a5c0ae10d26d2426415b3a54f30926d400458af
vsjostro <vsjostro@abo.fi>
2019-01-24 17:24:54 +0200

    added the production build to gh-pages

--- README.md
+++
@@ -1,3 +0,0 @@
-Stock Portfolio Management System
-
-https://it-teaching-abo-akademi.github.io/2018-interactive-web-apps-project-vsjostro/
---
+++ precache-manifest.ad7505d89f9943034edbd53fca443491.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "31ed1e18fb53c548af3a",
+    "url": "/2018-interactive-web-apps-project-vsjostro/static/js/runtime~main.31ed1e18.js"
+  },
+  {
+    "revision": "c8802ef9dd37e2e86ad0",
+    "url": "/2018-interactive-web-apps-project-vsjostro/static/js/main.c8802ef9.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/2018-interactive-web-apps-project-vsjostro/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "c8802ef9dd37e2e86ad0",
+    "url": "/2018-interactive-web-apps-project-vsjostro/static/css/main.677f8d03.chunk.css"
+  },
+  {
+    "revision": "6a3781cc70d16f01c73eeccca9bdfadd",
+    "url": "/2018-interactive-web-apps-project-vsjostro/index.html"
+  }
+];
